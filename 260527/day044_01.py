c68db86c768bdead5bbc2706ad06997e66bdaa26
inc6f4i <sm4inc@gmail.com>
2026-05-27 15:02:38 +0900

    Initial commit - 수업용 저장소 시작

---
+++ 260527/day044_01.py
@@ -0,0 +1,8 @@
+print("hello python!!!")# 이 라인의 print()함수는 리터럴을 출력시킴
+text = 'hello python'
+print(text)# 이 줄의 text는 변수임 즉 이 라인의 print()함수는 변수를 출력시킴 
+a = 1
+b = 10
+sum = a + b
+print(sum) # 이 줄의 sum = a + b 는 연산식임 = 오른쪽의 연산식의 결과를 = 왼쪽의 변수에 할당함 
+#print(a+b) # 이 줄의 a+ b는 연산식임 즉 이 라인의 print()함수는 연산식의 결과를 출력시킴 
---
+++ 260527/day044_02.py
@@ -0,0 +1,14 @@
+_myname='samshang'
+my_name='홍길동'
+MyName3='hong-gil-dong'
+country='대한민국'
+Country='korea'
+print(_myname)
+print(my_name)
+print(MyName3)
+print(country)
+print(Country)
+
+import keyword
+print(keyword.kwlist)
+
