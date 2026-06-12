8b3eb8f202e27ee300000bfda2aed311c830809b
inc6f4i <sm4inc@gmail.com>
2026-06-12 16:24:37 +0900

    메세지 입력해야 커밋되는군..

--- 260612/bz tessColor.py
+++ 260612/bz tessColor.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # 1. 이미지 로드
-image = cv2.imread(r'C:\Users\user\Desktop\Screenshot_20260609_015116_O.jpg')
+image = cv2.imread(r'C:\Users\user\Desktop\Screenshot_20260612_161450_O.jpg')
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # 2. 색상별 고유 영역(Mask) 범위 지정 (실제 기기 아이콘 색상 기준)
@@ -20,17 +20,24 @@ upper_yellow = np.array([35, 255, 255])
 lower_sky = np.array([85, 100, 100])
 upper_sky = np.array([105, 255, 255])
 
+# 분홍색(재배치대상)
+lower_pink = np.array([140, 70, 70])
+upper_pink = np.array([170, 255, 255])
+
 # 3. 마스크 생성 및 픽셀 수 카운트
 mask_red = cv2.inRange(hsv, lower_red1, upper_red1) + cv2.inRange(hsv, lower_red2, upper_red2)
 mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
 mask_sky = cv2.inRange(hsv, lower_sky, upper_sky)
+mask_pink = cv2.inRange(hsv, lower_pink, upper_pink)
 
 # 각각의 색상이 차지하는 픽셀 개수 뽑기
 red_pixels = cv2.countNonZero(mask_red)
 yellow_pixels = cv2.countNonZero(mask_yellow)
 sky_pixels = cv2.countNonZero(mask_sky)
+pink_pixels = cv2.countNonZero(mask_pink)
 
 total_pixels = image.shape[0] * image.shape[1]
 print(f"🔴 고장(빨강): {red_pixels/total_pixels*100:.2f}%")
 print(f"🟡 방전(노랑): {yellow_pixels/total_pixels*100:.2f}%")
 print(f"🔵 정비(하늘): {sky_pixels/total_pixels*100:.2f}%")
+print(f"🌸 재뱃 (분홍): {pink_pixels/total_pixels*100:.2f}%")
